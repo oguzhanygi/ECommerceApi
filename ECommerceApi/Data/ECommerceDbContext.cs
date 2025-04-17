55aed5d2cf96108058e0bbd5c737315ef7f4a0e6
Oğuzhan Yıldız <142035453+oguzhanygi@users.noreply.github.com>
2025-04-17 18:02:34 +0300

    Added model classes for Product and Category, defined relations between them.

--- ECommerceApi/Data/ECommerceDbContext.cs
+++ ECommerceApi/Data/ECommerceDbContext.cs
@@ -1,8 +1,49 @@
+using System.Diagnostics;
+using ECommerceApi.Models.Categories;
+using ECommerceApi.Models.Customers;
+using ECommerceApi.Models.Orders;
+using ECommerceApi.Models.Products;
 using Microsoft.EntityFrameworkCore;
 
 namespace ECommerceApi.Data;
 
 public class ECommerceDbContext(DbContextOptions<ECommerceDbContext> options) : DbContext(options)
 {
-    
+    public DbSet<Category> Categories { get; set; }
+    // public DbSet<Order> Orders { get; set; }
+    public DbSet<Product> Products { get; set; }
+    // public DbSet<Customer> Customers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasMany(c => c.SubCategories)
+                .WithOne(c => c.ParentCategory)
+                .HasForeignKey(c => c.ParentCategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            entity.HasIndex(c => c.Name).IsUnique();
+            entity.HasIndex(c => c.ParentCategoryId);
+
+            entity.HasQueryFilter(c => !c.IsDeleted);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasOne(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            entity.HasIndex(p => p.Name);
+            entity.HasIndex(p => p.Brand);
+            entity.HasIndex(p => p.CategoryId);
+
+            entity.HasQueryFilter(p => !p.IsDeleted);
+
+            entity.Property(p => p.CreatedAt)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+        });
+    }
 }
---
+++ ECommerceApi/Models/Categories/Category.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using ECommerceApi.Models.Products;
+
+namespace ECommerceApi.Models.Categories;
+
+public class Category
+{
+    public required Guid Id { get; init; }
+    [MaxLength(128), MinLength(2)]
+    public required string Name { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; }
+    public bool IsDeleted { get; set; } = false;
+    public Guid? ParentCategoryId { get; set; }
+    public Category? ParentCategory { get; set; }
+    public ICollection<Category>? SubCategories { get; set; } = new List<Category>();
+    public ICollection<Product>? Products { get; set; } = new List<Product>();
+}
